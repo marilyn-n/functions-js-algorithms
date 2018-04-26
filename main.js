9fa8dcb8e841ff48990e0d220adecdde47bc856d
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-26 16:27:47 -0500

    main.js and README.md started

---
+++ main.js
@@ -0,0 +1,22 @@
+// 1. Write a JavaScript function that generates all combinations of a string. Go to the editor
+// Example string : 'dog'
+// Expected Output : d,do,dog,o,og,g
+
+// 2. Write a JavaScript function that reverse a number. Go to the editor
+// Example x = 32243;
+// Expected Output : 34223
+
+// 3. Write a JavaScript function that accepts a string as a parameter and find the longest word
+// within the string. Go to the editor
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
+
+// 4. Write a JavaScript function that returns a passed string with letters in alphabetical order.
+// Go to the editor
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+// 5. Write a JavaScript function which accepts an argument and returns the type. Go to the editor
+// Note : There are six possible values that typeof returns: object, boolean, function, number,
+// string and undefined.
