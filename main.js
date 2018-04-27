065a7678274c57db65761eb16f1e335210963844
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-27 15:24:54 -0500

    function type of value completed

--- main.js
+++ main.js
@@ -10,7 +10,20 @@ const reverseNumbers = (number) => {
 
 reverseNumbers(32243);
 
-// 2. Write a JavaScript function that generates all combinations of a string.
+// 2. Write a JavaScript function which accepts an argument and returns the type.
+// Note : There are six possible values that typeof returns: object, boolean, function, number,
+// string and undefined.
+
+const typeOfExpression = value => typeof value;
+
+console.log(typeOfExpression({})); // object
+console.log(typeOfExpression(false)); // boolean
+console.log(typeOfExpression(() => {})); // function
+console.log(typeOfExpression(34)); // number
+console.log(typeOfExpression('hello world')); // string
+console.log(typeOfExpression()); // undefined
+
+// 3. Write a JavaScript function that generates all combinations of a string.
 // Example string : 'dog'
 // Expected Output : d,do,dog,o,og,g
 
@@ -19,16 +32,12 @@ reverseNumbers(32243);
 // stringCombinations('dog');
 
 
-// 3. Write a JavaScript function that accepts a string as a parameter and find the longest word
+// 4. Write a JavaScript function that accepts a string as a parameter and find the longest word
 // within the string.
 // Example string : 'Web Development Tutorial'
 // Expected Output : 'Development'
 
-// 4. Write a JavaScript function that returns a passed string with letters in alphabetical order.
+// 5. Write a JavaScript function that returns a passed string with letters in alphabetical order.
 // Example string : 'webmast er'
 // Expected Output : 'abeemrstw'
 // Assume punctuation and numbers symbols are not included in the passed string.
-
-// 5. Write a JavaScript function which accepts an argument and returns the type.
-// Note : There are six possible values that typeof returns: object, boolean, function, number,
-// string and undefined.
